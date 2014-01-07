b23b5aef7e42302d523ac2627a054c2d6290e8ce
Jan Rudovsky <jrudovsky@vendavo.com>
2014-01-07 14:21:28 +0100

    Initial

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-highcharts-tooltip-delay
+Highcharts tooltip delay
 ========================
 
 Add delay before displaying Highcharts Tooltip
+
+You can parametrize duration (default value is 1000)
+
+`var chartOptions = {tooltip: { delayForDisplay: 2000 }}`
---
+++ tooltip-delay.js
@@ -0,0 +1,30 @@
+(function (H) {
+
+    H.wrap(H.Point.prototype, 'onMouseOver', function (proceed) {
+
+        var point = this,
+            series = point.series,
+            chart = series.chart,
+            tooltip = chart.tooltip;
+
+        point.delayForDisplay = chart.options.tooltip.delayForDisplay || 1000;
+
+        proceed.apply(point, Array.prototype.slice.call(arguments, 1));
+    });
+
+    H.wrap(H.Tooltip.prototype, 'refresh', function (proceed) {
+
+        var point = arguments[1];
+        var chart = this.chart;
+        var tooltip = this;
+        var refreshArguments = arguments;
+
+        window.setTimeout(function () {
+            if (point === chart.hoverPoint) {
+                proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
+            }
+        }, chart.options.tooltip.delayForDisplay || 1000);
+
+    });
+
+}(Highcharts));
