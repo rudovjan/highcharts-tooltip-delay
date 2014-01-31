5deb09dc789f1e21c4cd677a1b138cf3218212d2
Jan Rudovsky <rudovsky@gmail.com>
2014-01-31 10:07:39 +0100

    Updated to last version 1.1

--- tooltip-delay.js
+++ tooltip-delay.js
@@ -1,30 +1,23 @@
 (function (H) {
 
-    H.wrap(H.Point.prototype, 'onMouseOver', function (proceed) {
-
-        var point = this,
-            series = point.series,
-            chart = series.chart,
-            tooltip = chart.tooltip;
-
-        point.delayForDisplay = chart.options.tooltip.delayForDisplay || 1000;
-
-        proceed.apply(point, Array.prototype.slice.call(arguments, 1));
-    });
-
     H.wrap(H.Tooltip.prototype, 'refresh', function (proceed) {
+
 
         var point = arguments[1];
         var chart = this.chart;
         var tooltip = this;
         var refreshArguments = arguments;
+        var delayForDisplay = chart.options.tooltip.delayForDisplay;
 
-        if(point.delayForDisplay){
+
+        if (delayForDisplay) {
             window.setTimeout(function () {
-                if (point === chart.hoverPoint) {
+
+                if (point === chart.hoverPoint || $.inArray(chart.hoverPoint, point)) {
                     proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
                 }
-            }, point.delayForDisplay || 1000);
+
+            }, delayForDisplay || 1000);
         } else {
             proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
         }
