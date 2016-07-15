079dcc59c0256160466ce2b88f5a7619f1cbe756
Jan Rudovsky <jrudovsky@vendavo.com>
2016-07-14 18:40:18 -0700

    ID for delayed tooltip in shared tooltip

--- tooltip-delay.js
+++ tooltip-delay.js
@@ -1,31 +1,37 @@
-(function (H) {
+(function(H) {
 
-    var timerId = {};
+  var timerId = {};
 
-    H.wrap(H.Tooltip.prototype, 'refresh', function (proceed) {
+  H.wrap(H.Tooltip.prototype, 'refresh', function(proceed) {
 
-        var seriesName = arguments[1].series.name;
+    var seriesName;
+    if (Array.isArray(arguments[ 1 ])) {
+      // Can be array in case that, it's shared tooltip
+      seriesName = "not_have_now_clear_id";
+    } else {
+      seriesName = arguments[ 1 ].series.name;
+    }
 
-        var delayForDisplay = this.chart.options.tooltip.delayForDisplay ? this.chart.options.tooltip.delayForDisplay : 1000;
+    var delayForDisplay = this.chart.options.tooltip.delayForDisplay ? this.chart.options.tooltip.delayForDisplay : 1000;
 
-        if (timerId[seriesName]) {
-            clearTimeout(timerId[seriesName]);
-            delete timerId[seriesName];
-        }
+    if (timerId[ seriesName ]) {
+      clearTimeout(timerId[ seriesName ]);
+      delete timerId[ seriesName ];
+    }
 
-        timerId[seriesName] = window.setTimeout(function () {
-            var point = this.refreshArguments[0];
+    timerId[ seriesName ] = window.setTimeout(function() {
+      var point = this.refreshArguments[ 0 ];
 
-            if (point === this.chart.hoverPoint || $.inArray(this.chart.hoverPoint, point) > -1) {
-                proceed.apply(this.tooltip, this.refreshArguments);
-            }
+      if (point === this.chart.hoverPoint || $.inArray(this.chart.hoverPoint, point) > -1) {
+        proceed.apply(this.tooltip, this.refreshArguments);
+      }
 
-        }.bind({
-            refreshArguments: Array.prototype.slice.call(arguments, 1),
-            chart: this.chart,
-            tooltip: this
-        }), delayForDisplay);
+    }.bind({
+      refreshArguments: Array.prototype.slice.call(arguments, 1),
+      chart: this.chart,
+      tooltip: this
+    }), delayForDisplay);
 
-    });
+  });
 
 }(Highcharts));
