f8aaf237feb00f03af0c6e8d15f4c7f4acbdd542
Brian Henry <brian.henry@ni.com>
2021-03-24 16:51:49 -0500

    Revert "convert to typescript, add build tooling, update license info, etc"
    This reverts commit d0dab89e06153b0c205fa2c8f5a5bf2fb78587dc.

---
+++ tooltip-delay.dist.js
@@ -0,0 +1,65 @@
+(function (factory) {
+  "use strict";
+
+  if (typeof module === "object" && module.exports) {
+    module.exports = factory;
+  } else {
+    factory(Highcharts);
+  }
+}(function (H) {
+
+  let timerId = {};
+
+  const generatePointsUniqueKey = (points) => {
+
+    const generatePointKey = (point) => {
+      return point.category + " " + point.series.name + ": " + point.x + " " + point.y;
+    };
+
+    const result = points.map(generatePointKey).join(', ');
+
+    return result;
+  }
+
+  function inArray(value, arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (this[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  H.wrap(H.Tooltip.prototype, 'refresh', function (proceed) {
+    let seriesName;
+
+    if (Array.isArray(arguments[1])) {
+      // Can be array in case that, it's shared tooltip
+      seriesName = generatePointsUniqueKey(arguments[1]);
+    } else {
+      seriesName = arguments[1].series.name;
+    }
+
+    const delayForDisplay = this.chart.options.tooltip.delayForDisplay ? this.chart.options.tooltip.delayForDisplay : 1000;
+
+    if (timerId[seriesName]) {
+      clearTimeout(timerId[seriesName]);
+      delete timerId[seriesName];
+    }
+
+    timerId[seriesName] = window.setTimeout(function () {
+      let pointOrPoints = this.refreshArguments[0];
+
+      if (pointOrPoints === this.chart.hoverPoint || inArray(this.chart.hoverPoint, pointOrPoints) > -1) {
+        proceed.apply(this.tooltip, this.refreshArguments);
+      }
+
+    }.bind({
+      refreshArguments: Array.prototype.slice.call(arguments, 1),
+      chart: this.chart,
+      tooltip: this
+    }), delayForDisplay);
+
+  });
+
+}));
